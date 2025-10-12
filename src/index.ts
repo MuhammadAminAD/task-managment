ce941126a69c81af00006dafa4984b9515471abd
Muhammad-amin7 <abduhalilovshohruh681@gmail.com>
2025-10-12 20:50:47 +0500

    Cors ochildi

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: "http://localhost",
+    origin: "http://localhost:5173",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 204
